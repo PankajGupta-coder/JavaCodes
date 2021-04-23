07fd514bd5a4d6201bf654ce0b1a4680d84e5a00
PankajGupta-coder <pg766399@gmail.com>
2021-04-23 11:10:35 +0530

    new commit

--- main.java
+++ main.java
@@ -1,21 +1,20 @@
 class main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // Array shorting in accending order
-        int a[] = {45, 2, 33, 874, 74, 65, 55, 56, 46, 63, 27, 17, 72, 37, 74 };
-        int swap, num ;
-        for (int i=0; i < a.length; i++) {
-           for ( int j = i ;j < a.length ; j ++) {
+        int a[] = { 45, 2, 33, 874, 74, 65, 55, 56, 46, 63, 27, 17, 72, 37, 74 };
+        int swap;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j < a.length; j++) {
                 if (a[i] > a[j]) {
-                }
-                else {swap = a[j];
+                } else {
+                    swap = a[j];
                     a[j] = a[i];
-
                     a[i] = swap;
                 }
-           } 
-        } 
-        for (int i = 0; i< a.length; i++) {
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
-        }   
-    } 
+        }
+    }
 }
